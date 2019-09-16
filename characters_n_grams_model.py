50adb8775c6ebb961dd03751df46bf659645e900
Gowtham Behara <gothsiddu@gmail.com>
2019-09-16 22:43:48 +0530

    Define func to construct n gram model

--- characters_n_grams_model.py
+++ characters_n_grams_model.py
@@ -13,7 +13,20 @@ def clean_text(text):
     return re.sub(r'[^A-Za-z. ]', '', text)
 
 
+def get_character_n_gram_model(text, n=3):
+    n_grams = {}
+    for i in range(len(text) - n):
+        seq = text[i, i + n]
+        if seq not in n_grams.keys():
+            n_grams[seq] = []
+
+        n_grams[seq].append(text[i + n])
+    return n_grams
+
+
 if __name__ == '__main__':
     URL = "https://en.wikipedia.org/wiki/Tennis"
     article_text = get_text_from_url(URL)
     article_text = clean_text(article_text)
+
+    n_grams = get_character_n_gram_model(article_text)
