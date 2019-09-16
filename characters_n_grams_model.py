09f9bb96a372bad72330084eb2c64e38d396a4c4
Gowtham Behara <gothsiddu@gmail.com>
2019-09-16 22:59:36 +0530

    Define func to generate predictive text based on input seq for n grams

--- characters_n_grams_model.py
+++ characters_n_grams_model.py
@@ -1,12 +1,12 @@
 from urllib import request
 from bs4 import BeautifulSoup
-import re
+import re, random
 
 
 def get_text_from_url(url):
     raw_html = request.urlopen(url).read()
-    article_html = BeautifulSoup(raw_html, 'lxml')
-    return ''.join(article_html.find_all('p')).lower()
+    article_html = BeautifulSoup(raw_html, 'html.parser')
+    return ''.join([paragraph.text for paragraph in article_html.find_all('p')]).lower()
 
 
 def clean_text(text):
@@ -16,12 +16,28 @@ def clean_text(text):
 def get_character_n_gram_model(text, n=3):
     n_grams = {}
     for i in range(len(text) - n):
-        seq = text[i, i + n]
+        seq = text[i: i + n]
         if seq not in n_grams.keys():
             n_grams[seq] = []
 
         n_grams[seq].append(text[i + n])
     return n_grams
+
+
+def generate_predictive_text(text, n_grams, n=3):
+    curr_seq = text[:n]
+    output = curr_seq
+
+    for i in range(200):
+        if curr_seq not in n_grams.keys():
+            break
+
+        possible_chars = n_grams[curr_seq]
+        next_char = possible_chars[random.randrange(len(possible_chars))]
+        output = output + next_char
+        curr_seq = text[i + 1: len(output)]
+
+    return output
 
 
 if __name__ == '__main__':
@@ -30,3 +46,5 @@ if __name__ == '__main__':
     article_text = clean_text(article_text)
 
     n_grams = get_character_n_gram_model(article_text)
+    predictive_text = generate_predictive_text(article_text, n_grams)
+    print(predictive_text)
