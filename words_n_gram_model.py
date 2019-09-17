fb0ee73444cf3fd699fea738c221188b61b74116
Gowtham Behara <gothsiddu@gmail.com>
2019-09-17 12:01:38 +0530

    Define func to get n_gram_model by words

--- words_n_gram_model.py
+++ words_n_gram_model.py
@@ -1,6 +1,6 @@
 from urllib import request
 from bs4 import BeautifulSoup
-import re
+import re, nltk
 
 
 def get_text_from_url(url):
@@ -13,8 +13,23 @@ def clean_text(text):
     return re.sub(r'[^A-Za-z. ]', '', text)
 
 
+def get_n_grams_model(text, n=3):
+    n_grams = {}
+    words = nltk.word_tokenize(text)
+    for i in range(len(words) - n):
+        seq = ' '.join(words[i: i + n])
+        if seq not in n_grams.keys():
+            n_grams[seq] = []
+
+        n_grams[seq].append(words[i + n])
+
+    return n_grams
+
+
 if __name__ == '__main__':
     URL = "https://en.wikipedia.org/wiki/Tennis"
     article_text = get_text_from_url(URL)
     article_text = clean_text(article_text)
-    print(article_text)
+
+    n_grams_model = get_n_grams_model(article_text)
+    print(n_grams_model)
