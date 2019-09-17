dd1404c6b07983bfcd13a371ebe241ac8fac7664
Gowtham Behara <gothsiddu@gmail.com>
2019-09-17 12:14:39 +0530

    Define func to generate predictive text from n_gram_model constructed

--- words_n_gram_model.py
+++ words_n_gram_model.py
@@ -1,6 +1,6 @@
 from urllib import request
 from bs4 import BeautifulSoup
-import re, nltk
+import re, nltk, random
 
 
 def get_text_from_url(url):
@@ -23,7 +23,24 @@ def get_n_grams_model(text, n=3):
 
         n_grams[seq].append(words[i + n])
 
-    return n_grams
+    return n_grams, words
+
+
+def generate_predictive_text(words, n_gram_model, n=3):
+    curr_seq = ' '.join(words[:n])
+    output = curr_seq
+    seq_words = nltk.word_tokenize(output)
+    for i in range(200):
+        if curr_seq not in n_grams_model.keys():
+            break
+
+        possible_words = n_grams_model[curr_seq]
+        next_word = possible_words[random.randrange(len(possible_words))]
+        output = output + ' ' + next_word
+        seq_words = nltk.word_tokenize(output)
+        curr_seq = ' '.join(seq_words[i + 1: len(seq_words)])
+
+    return seq_words
 
 
 if __name__ == '__main__':
@@ -31,5 +48,6 @@ if __name__ == '__main__':
     article_text = get_text_from_url(URL)
     article_text = clean_text(article_text)
 
-    n_grams_model = get_n_grams_model(article_text)
-    print(n_grams_model)
+    n_grams_model, words = get_n_grams_model(article_text)
+    predictive_text = generate_predictive_text(words, n_grams_model)
+    print(predictive_text)
